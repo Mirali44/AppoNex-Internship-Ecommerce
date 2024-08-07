ce617812d2dc7936b4e495770a3d8419aea7b4b4
Mirali <112255961+Mirali44@users.noreply.github.com>
2024-08-07 08:56:46 +0400

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -1,30 +1,22 @@
 import fs from 'node:fs/promises';
 import bodyParser from 'body-parser';
 import express from 'express';
-import cors from 'cors';
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(express.static('public'));
 
-// Configure CORS options
-const corsOptions = {
-  origin: 'https://your-frontend-domain.com', // Replace with your actual frontend domain
-  methods: 'GET,POST,OPTIONS',
-  allowedHeaders: 'Content-Type,Authorization',
-};
-
-// Enable CORS with the specified options
-app.use(cors(corsOptions));
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
 
 app.get('/meals', async (req, res) => {
-  try {
-    const meals = await fs.readFile('./data/available-meals.json', 'utf8');
-    res.json(JSON.parse(meals));
-  } catch (error) {
-    res.status(500).json({ message: 'Internal Server Error' });
-  }
+  const meals = await fs.readFile('./data/available-meals.json', 'utf8');
+  res.json(JSON.parse(meals));
 });
 
 app.use((req, res) => {
@@ -34,3 +26,4 @@ app.use((req, res) => {
   res.status(404).json({ message: 'Not found' });
 });
 
+app.listen(3000)
